60aeaceff7c3247c584bba5327e1dc1affa13f75
Nydia Tapia <tapian@vcu.edu>
2019-10-09 22:02:46 -0400

    Initial Commit

---
+++ INFO 450 - Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INFO 450 - Assignment2
+//
+//  Created by Nydia Tapia on 10/9/19.
+//  Copyright © 2019 Nydia Tapia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
